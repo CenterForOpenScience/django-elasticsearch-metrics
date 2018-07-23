ba9c7226c070e8fa324f6a680f1a88416a096cb1
Steven Loria <sloria1@gmail.com>
2018-07-23 18:01:22 -0400

    We don't need middleware

--- tests/settings.py
+++ tests/settings.py
@@ -4,16 +4,7 @@ SECRET_KEY = "not so secret in tests"
 DEBUG = True
 ALLOWED_HOSTS = []
 INSTALLED_APPS = ["elasticsearch_metrics"]
-MIDDLEWARE_CLASSES = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+MIDDLEWARE_CLASSES = []
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 TIME_ZONE = "UTC"
 ELASTICSEARCH_DSL = {
