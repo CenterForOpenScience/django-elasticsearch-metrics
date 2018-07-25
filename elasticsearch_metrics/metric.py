05806527023233543676ca2057584f0cd608c643
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-07-25 15:27:37 -0400

    Flesh out Metric class a bit
    Add Metric.get_index_template and Metric.get_index_name

--- elasticsearch_metrics/metric.py
+++ elasticsearch_metrics/metric.py
@@ -1,9 +1,58 @@
-from elasticsearch_dsl import Document, Date
+from django.utils import timezone
+from django.utils.six import add_metaclass
+from elasticsearch_dsl import Document, Date, IndexTemplate
+from elasticsearch_dsl.document import DocumentMeta, DocumentOptions
 
 
+class MetricOptions(DocumentOptions):
+    """Adds template_name and template to
+    available options.
+    """
+
+    def __init__(self, name, bases, attrs):
+        meta = attrs.get("Meta", None)
+        super(MetricOptions, self).__init__(name, bases, attrs)
+        # TODO: Automatically comput template_name and template intead of defaulting to None
+        self.template_name = getattr(meta, "template_name", None)
+        self.template = getattr(meta, "template", None)
+
+
+class MetricMeta(DocumentMeta):
+    """Metaclass for the base `Metric` class."""
+
+    def __new__(cls, name, bases, attrs):
+        attrs["_doc_type"] = MetricOptions(name, bases, attrs)
+        # We can't call super because DocumentMeta.__new__ instantiates DocumentOptions,
+        # which pops off Meta, which we want to read from in
+        # MetricOptions.
+        return type.__new__(cls, name, bases, attrs)
+
+
+# Python only allows for one metaclass to be selected.
+# Create a metaclass that is the subclass of both DocumentMeta and MetricMeta
+# https://stackoverflow.com/questions/38403795/python-metaclass-conflict-type-error/38404006#38404006
+MetricMeta = type("MetricMeta", (type(Document), MetricMeta), {})
+
+
+@add_metaclass(MetricMeta)
 class Metric(Document):
     timestamp = Date()
 
     @classmethod
+    def create_index_template(cls):
+        pass
+
+    @classmethod
     def get_index_template(cls):
-        assert 0, "todo"
+        # TODO: mapping
+        return IndexTemplate(
+            name=cls._doc_type.template_name, template=cls._doc_type.template
+        )
+
+    @classmethod
+    def get_index_name(cls, date=None):
+        date = date or timezone.now().date()
+        # TODO: Make dateformat configurable
+        dateformat = "%Y.%m.%d"
+        date_formatted = date.strftime(dateformat)
+        return "{}-{}".format(cls._doc_type.template_name, date_formatted)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,11 +1,57 @@
+import pytest
+import datetime as dt
+
+from django.utils import timezone
 from elasticsearch_metrics import Metric
-from elasticsearch_dsl import IndexTemplate
+from elasticsearch_dsl import IndexTemplate, connections
+
+
+@pytest.fixture()
+def client():
+    return connections.get_connection()
 
 
 class PreprintView(Metric):
     class Meta:
-        template_name = "osf_metrics-preprint-views"
+        # TODO: Make this unnecessary and compute this.
+        template_name = "osf_metrics_preprintviews"
+        template = "osf_metrics_preprintviews-*"
 
 
-def test_index_template():
-    assert isinstance(PreprintView.get_index_template(), IndexTemplate)
+def test_get_index_template():
+    template = PreprintView.get_index_template()
+    assert isinstance(template, IndexTemplate)
+    assert template._template_name == "osf_metrics_preprintviews"
+    assert "osf_metrics_preprintviews-*" in template.to_dict()["index_patterns"]
+
+
+def test_get_index_template_returns_template_with_correct_mapping():
+    assert 0, "todo"
+
+
+def test_get_index_name():
+    date = dt.date(2020, 2, 14)
+    assert (
+        PreprintView.get_index_name(date=date) == "osf_metrics_preprintviews-2020.02.14"
+    )
+
+
+def test_get_index_name_gets_index_for_today_by_default():
+    today = timezone.now().date()
+    # TODO: should be in settings
+    dateformat = "%Y.%m.%d"
+    today_formatted = today.strftime(dateformat)
+    assert PreprintView.get_index_name() == "osf_metrics_preprintviews-{}".format(
+        today_formatted
+    )
+
+
+@pytest.mark.es
+def test_create_metric_creats_template_with_mapping(client):
+    PreprintView.create_index_template()
+
+    template = client.indices.get_template(name="osf_metrics-preprint-views")
+    mappings = template["osf_metrics-preprint-views"]["mappings"]
+    properties = mappings["_default_"]["properties"]
+    assert "timestamp" in properties
+    assert properties["timestamp"] == {"doc_values": True, "type": "date"}
