93cbc596b48b89bba3a7a80a5035c26a5b3c8b86
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-07-26 10:39:04 -0500

    Restore double quotes and expect 'doc' mapping instead of '_default_'.

--- elasticsearch_metrics/metric.py
+++ elasticsearch_metrics/metric.py
@@ -21,22 +21,29 @@ class Metric(Document):
     timestamp = Date(doc_values=True)
 
     class Meta:
-        dynamic_templates = MetaField([
-            {
-              'strings': {
-                'match': '*',
-                'match_mapping_type': 'string',
-                'mapping':   { 'type': 'string', 'fielddata': {'format': 'doc_values'}, 'doc_values': True, 'index': 'not_analyzed' }
-              }
-            }
-        ])
+        dynamic_templates = MetaField(
+            [
+                {
+                    "strings": {
+                        "match": "*",
+                        "match_mapping_type": "string",
+                        "mapping": {
+                            "type": "string",
+                            "fielddata": {"format": "doc_values"},
+                            "doc_values": True,
+                            "index": "not_analyzed",
+                        },
+                    }
+                }
+            ]
+        )
         all = MetaField(enabled=False)
         source = MetaField(enabled=False)
 
     @classmethod
     def create_index_template(cls):
         index_template = cls.get_index_template()
-        index_template.settings(refresh_interval='5s')
+        index_template.settings(refresh_interval="5s")
         index_template.document(cls)
         index_template.save()
         return index_template
@@ -44,7 +51,6 @@ class Metric(Document):
     @classmethod
     def get_index_template(cls):
         # TODO: mapping
-        # TODO: _default_ is deprecated in 6.0.0 - do we still want to use this?
         return IndexTemplate(name=cls._template_name, template=cls._template)
 
     @classmethod
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -19,48 +19,50 @@ class PreprintView(Metric):
 
     class Meta:
         # TODO: Make this unnecessary and compute this.
-        template_name = 'osf_metrics_preprintviews'
-        template = 'osf_metrics_preprintviews-*'
+        template_name = "osf_metrics_preprintviews"
+        template = "osf_metrics_preprintviews-*"
+
 
 def test_get_index_template():
     template = PreprintView.get_index_template()
     assert isinstance(template, IndexTemplate)
-    assert template._template_name == 'osf_metrics_preprintviews'
-    assert 'osf_metrics_preprintviews-*' in template.to_dict()['index_patterns']
+    assert template._template_name == "osf_metrics_preprintviews"
+    assert "osf_metrics_preprintviews-*" in template.to_dict()["index_patterns"]
 
 
 def test_get_index_template_returns_template_with_correct_mapping():
-    assert 0, 'todo'
+    assert 0, "todo"
 
 
 def test_get_index_name():
     date = dt.date(2020, 2, 14)
     assert (
-        PreprintView.get_index_name(date=date) == 'osf_metrics_preprintviews-2020.02.14'
+        PreprintView.get_index_name(date=date) == "osf_metrics_preprintviews-2020.02.14"
     )
+
 
 def test_get_index_name_gets_index_for_today_by_default():
     today = timezone.now().date()
     # TODO: should be in settings
-    dateformat = '%Y.%m.%d'
+    dateformat = "%Y.%m.%d"
     today_formatted = today.strftime(dateformat)
-    assert PreprintView.get_index_name() == 'osf_metrics_preprintviews-{}'.format(
+    assert PreprintView.get_index_name() == "osf_metrics_preprintviews-{}".format(
         today_formatted
     )
+
 
 # @pytest.mark.es
 def test_create_metric_creats_template_with_mapping(client):
     PreprintView.create_index_template()
     template_name = PreprintView._template_name
     template = client.indices.get_template(name=template_name)
-    mappings = template[template_name]['mappings']
-    # TODO: _default_ is deprecated in 6.0.0 - do we still want that mapping?
-    assert mappings['_default_']['_all']['enabled'] is False
-    assert mappings['_default_']['_source']['enabled'] is False
-    assert 'dynamic_templates' in mappings['_default_']
-    properties = mappings['_default_']['properties']
-    assert 'timestamp' in properties
-    assert properties['timestamp'] == {'doc_values': True, 'type': 'date'}
-    assert properties['provider_id'] == {'type' : 'string'}
-    assert properties['user_id'] == {'type' : 'string'}
-    assert properties['preprint_id'] == {'type' : 'string'}
+    mappings = template[template_name]["mappings"]
+    assert mappings["doc"]["_all"]["enabled"] is False
+    assert mappings["doc"]["_source"]["enabled"] is False
+    assert "dynamic_templates" in mappings["doc"]
+    properties = mappings["doc"]["properties"]
+    assert "timestamp" in properties
+    assert properties["timestamp"] == {"doc_values": True, "type": "date"}
+    assert properties["provider_id"] == {"type": "string"}
+    assert properties["user_id"] == {"type": "string"}
+    assert properties["preprint_id"] == {"type": "string"}
