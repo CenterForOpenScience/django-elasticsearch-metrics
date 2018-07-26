29c14c47703fee4df14f839b87119efdc02f02ea
Steven Loria <sloria1@gmail.com>
2018-07-26 11:50:18 -0400

    Remove old tests and mark expected failures
    NOTE: There is still one legitimate failure

--- tests/test_management_commands/test_sync_metrics.py
+++ tests/test_management_commands/test_sync_metrics.py
@@ -1,12 +1,14 @@
+import pytest
 from elasticsearch_metrics.management.commands.sync_metrics import Command
 
 
 def test_without_args(capsys):
     cmd = Command()
-    cmd.run_from_argv(['manage.py', 'sync_metrics'])
+    cmd.run_from_argv(["manage.py", "sync_metrics"])
     out, err = capsys.readouterr()
-    assert 'Synchronized metrics.' in out
+    assert "Synchronized metrics." in out
 
 
+@pytest.mark.xfail
 def test_synchronizes_all_metrics():
-    assert 0, 'todo'
+    assert 0, "todo"
