779e9ad67110153462dd7640e01b88b967aaab47
Abram Booth <boothaa@gmail.com>
2022-09-07 16:13:51 -0400

    more pythons and more djangoes

--- elasticsearch_metrics/signals.py
+++ elasticsearch_metrics/signals.py
@@ -1,10 +1,10 @@
 from django.dispatch import Signal
 
 # Sent before index template is created in elasticsearch
-pre_index_template_create = Signal(providing_args=["index_template", "using"])
+pre_index_template_create = Signal()
 # Sent after index template is created in elasticsearch
-post_index_template_create = Signal(providing_args=["index_template", "using"])
+post_index_template_create = Signal()
 # Sent at the beginning of Metric's save()
-pre_save = Signal(providing_args=["instance", "using", "index"])
+pre_save = Signal()
 # Like pre_save, but sent at the end of the save() method
-post_save = Signal(providing_args=["instance", "using", "index"])
+post_save = Signal()
